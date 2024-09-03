2aa671311479281b9622be33a6bf380f3c07ea95
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-09-03 13:08:44 +0100

    connection and actual reading of device

---
+++ Code/Micropython/main.py
@@ -0,0 +1,26 @@
+from BT_periphral import *
+import bluetooth
+
+def demo():
+    ble = bluetooth.BLE()
+    p = BLESimplePeripheral(ble)
+
+    def on_rx(v):
+        print("RX", v)
+
+    p.on_write(on_rx)
+
+    i = 0
+    while True:
+        if p.is_connected():
+            # Short burst of queued notifications.
+            for _ in range(3):
+                data = str(i) + "_"
+                #print("TX", data)
+                p.send("data")
+                i += 1
+        time.sleep_ms(100)
+
+
+if __name__ == "__main__":
+    demo()
